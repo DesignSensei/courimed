f027a40cb24c2745e8968fc0242e876af7c366ba
zuke <obilomizuchukwu@gmail.com>
2025-06-05 18:16:59 +0100

    Imported color and used in root file

--- courimed-mobile/app/index.tsx
+++ courimed-mobile/app/index.tsx
@@ -3,6 +3,7 @@ import { Image, View } from "react-native";
 import { useRouter, Router } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { colors } from "./constants/colors"
 
 export default function Index() {
   const router: Router = useRouter();
@@ -18,7 +19,7 @@ export default function Index() {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#dd5d05" }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: colors.primary[500] }}>
         <Image
           source={require("../assets/images/courimed-icon-white.png")}
           style={{ width: "30%", height: undefined, aspectRatio: 1, resizeMode: "contain" }}
